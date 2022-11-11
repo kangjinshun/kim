3b38597cafbfc1a6ef6f96377fd0e62f38111a97
kangjinshun <36021068+kangjinshun@users.noreply.github.com>
2022-11-11 18:28:17 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -24,7 +24,7 @@ var S = {
     if (i !== -1) {
       S.UI.simulate(decodeURI(action).substring(i + 3));
     } else {
-      S.UI.simulate('|#countdown 3||李四斤|我|想你了|❤|#rectangle|');
+      S.UI.simulate('|#countdown 3||四斤|我|想你了|❤|#rectangle|');
     }
 
     S.Drawing.loop(function () {
